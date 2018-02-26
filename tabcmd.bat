1cf701efb7b82d296f8ec9b5139ad6667f298e1b
brahmareddy09 <bramhareddy09@gmail.com>
2018-02-26 17:19:19 +0000

    sdd

--- tabcmd.bat
+++ tabcmd.bat
@@ -1,2 +1,2 @@
 cd C:\Program Files\Tableau\Tableau Server\10.5\bin
-tabcmd login -s https://dev.tableau.wb.com -u Brahmareddy.Lankapothu -p Tableau@123
+tabcmd login -s https://dev.tableau.wb.com -t Sandbox Testing -u Brahmareddy.Lankapothu -p Tableau@123
