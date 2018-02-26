a271f4fc3ab35241398ef07ccc35fced1f5b8ec8
brahmareddy09 <bramhareddy09@gmail.com>
2018-02-26 15:20:28 +0000

    add tabcmd

--- tabcmd.bat
+++ tabcmd.bat
@@ -1,2 +1,2 @@
-C:\Program Files\Tableau\Tableau Server\10.5\bin
+cd C:\Program Files\Tableau\Tableau Server\10.5\bin
 tabcmd login -s https://dev.tableau.wb.com -u Brahmareddy.Lankapothu -p Tableau@123
